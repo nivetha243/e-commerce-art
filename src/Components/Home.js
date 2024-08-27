43041157a6ce1f4b59d967fb1731300791cad243
Varshini-Senthil2042 <varshiniusharani@gmail.com>
2024-08-27 15:18:09 +0530

    test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome Home Page</h1>
       <Link to="/about">Go About</Link>
     </div>
   );
